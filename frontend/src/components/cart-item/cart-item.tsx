1a94c756dd08380b8bb4d81676bce8af4fa29f14
Oluwafemi Anjorin <leelsmuth@Oluwafemis-MacBook-Pro.local>
2022-03-31 19:31:28 -0400

    cart item functionality

--- frontend/src/components/cart-item/cart-item.tsx
+++ frontend/src/components/cart-item/cart-item.tsx
@@ -1,11 +1,13 @@
 import { FC, ReactElement } from 'react';
-
+import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
+import { decrement, increment, removeItem } from '../../state/actionCreator';
 import './cart-item.styles.css';
 
 export interface ICartItem {
+  id: string;
   name: string;
   imageSrc: string;
-  quantity: number;
+  quantitySelected: number;
   price: number;
 }
 
@@ -14,15 +16,28 @@ interface ICartItemProps {
 }
 
 const CartItem: FC<ICartItemProps> = ({ cartItem }): ReactElement => {
-  const { name, imageSrc, quantity, price } = cartItem;
+  const dispatch = useDispatch();
+
+  const { name, imageSrc, quantitySelected, price } = cartItem;
   return (
     <div className="cart-item-container">
       <img src={imageSrc} />
       <div className="cart-item-details">
         <span>{name} </span>
-        <span> Quantity: {quantity} </span>
+        <span> Quantity: {quantitySelected} </span>
         <span> price: ${price.toFixed(2)} </span>
       </div>
+      <div className="add-minus-quantity">
+              <i className="fas fa-minus minus" 
+              onClick={() => dispatch(decrement(cartItem))}></i>
+              {/* <input type="text" placeholder={quantitySelected} disabled /> */}
+              <i className="fas fa-plus add" onClick={() => dispatch(increment(cartItem))}></i>
+            </div>
+
+            <div className="remove-item">
+              <i className="fas fa-trash-alt remove" 
+              onClick={() => dispatch(removeItem(cartItem))}></i>
+            </div>
     </div>
   );
 };
