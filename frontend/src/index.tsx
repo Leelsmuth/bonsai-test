c4031a5fc319cf87b8da3a701d49b702488cc650
Oluwafemi Anjorin <leelsmuth@Oluwafemis-MacBook-Pro.local>
2022-03-31 19:27:47 -0400

    setup redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore, compose, applyMiddleware, Store } from "redux";
+import thunk from "redux-thunk";
+import {
+  productReducer, cartReducer
+} from "./state/reducer";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { CartProvider } from "./cart-context";
 
+const bigReducer = combineReducers({
+  productsList: productReducer,
+  cart: cartReducer
+});
+
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store: Store<any> = createStore(
+  bigReducer,
+  applyMiddleware(thunk)
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <Provider store={store}>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
